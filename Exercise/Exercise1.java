b5cb8b976e410f0774a5cc6af185625f1343b143
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-26 20:57:39 +0800

    Update Exercise1.java

--- Exercise/Exercise1.java
+++ Exercise/Exercise1.java
@@ -30,3 +30,34 @@ class Smiley {
     c.draw();
   }
 }
+
+// solution: 
+// import com.educative.graphics.*;
+
+// class Smiley {
+//   public static void main(String[] args) {
+//     Canvas c;
+//     c = new Canvas(200, 200);
+
+//     // Draw the outline of the face
+//     c.fill("yellow");
+//     c.stroke("black");
+//     c.circle(100, 100, 50);
+
+//     // draw the mouth
+//     c.stroke("black");
+//     c.fill("yellow");
+//     c.circle(100, 100, 30);
+//     c.stroke("yellow");
+//     c.rect(68, 68, 62, 40);
+
+//     // draw the eyes
+//     c.stroke("black");
+//     c.fill("black");
+//     c.circle(100 - 20, 100 - 10, 5);
+//     c.circle(100 + 20, 100 - 10, 5);
+
+//     c.draw();
+//   }
+// }
+
