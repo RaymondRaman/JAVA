348e7315089c63c56d7e83efd8f714f17e611964
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-26 21:08:08 +0800

    Update Exercise1.java

--- Exercise/Exercise1.java
+++ Exercise/Exercise1.java
@@ -1,3 +1,4 @@
+// Draw a smilling face
 import com.educative.graphics.*;
 
 class Smiley {
@@ -35,29 +36,65 @@ class Smiley {
 // import com.educative.graphics.*;
 
 // class Smiley {
-//   public static void main(String[] args) {
-//     Canvas c;
-//     c = new Canvas(200, 200);
+  public static void main(String[] args) {
+    Canvas c;
+    c = new Canvas(200, 200);
 
-//     // Draw the outline of the face
-//     c.fill("yellow");
-//     c.stroke("black");
-//     c.circle(100, 100, 50);
+    // Draw the outline of the face
+    c.fill("yellow");
+    c.stroke("black");
+    c.circle(100, 100, 50);
 
-//     // draw the mouth
-//     c.stroke("black");
-//     c.fill("yellow");
-//     c.circle(100, 100, 30);
-//     c.stroke("yellow");
-//     c.rect(68, 68, 62, 40);
+    // draw the mouth
+    c.stroke("black");
+    c.fill("yellow");
+    c.circle(100, 100, 30);
+    c.stroke("yellow");
+    c.rect(68, 68, 62, 40);
 
-//     // draw the eyes
-//     c.stroke("black");
-//     c.fill("black");
-//     c.circle(100 - 20, 100 - 10, 5);
-//     c.circle(100 + 20, 100 - 10, 5);
+    // draw the eyes
+    c.stroke("black");
+    c.fill("black");
+    c.circle(100 - 20, 100 - 10, 5);
+    c.circle(100 + 20, 100 - 10, 5);
 
-//     c.draw();
-//   }
-// }
+    c.draw();
+  }
+}
 
+// Exercise
+import com.educative.graphics.*;
+
+class SmileyFactored {
+  static Canvas c;
+
+  public static void drawOutline(){
+      c.fill("yellow");
+      c.stroke("black");
+      c.circle(100, 100, 50);
+  }
+
+  public static void drawMouth(){
+    c.stroke("black");
+    c.fill("yellow");
+    c.circle(100, 100, 30);
+    c.stroke("yellow");
+    c.rect(68, 68, 62, 40);
+  }
+
+  public static void drawEyes(){
+    c.stroke("black");
+    c.fill("black");
+    c.circle(100 - 20, 100 - 10, 5);
+    c.circle(100 + 20, 100 - 10, 5);
+  }
+
+  public static void main(String[] args) {
+    
+    c = new Canvas(200, 200);
+    drawOutline();
+    drawMouth();
+    drawEyes();
+    c.draw();
+  }
+}
