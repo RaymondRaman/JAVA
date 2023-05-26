e58c3aada64324e39ffa3074f9d6db4def5b9159
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-26 21:19:16 +0800

    Update Exercise1.java

--- Exercise/Exercise1.java
+++ Exercise/Exercise1.java
@@ -108,3 +108,14 @@ class Exercise{
 
     }
 }
+
+// Format
+class printExamples {
+  public static void main(String args[]) {
+    System.out.format("My favorite number is %.3f.\n", 3.141592654); 
+    
+    // part of a times table:
+    System.out.format("%2d %2d %2d\n", 4, 6, 8);
+    System.out.format("%2d %2d %2d\n", 8, 12, 16);
+  }
+}
