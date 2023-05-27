ba8f02118970b405660fbed662b124ebd0b89a50
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-27 10:33:52 +0800

    Update Exercise1.java

--- Exercise/Exercise1.java
+++ Exercise/Exercise1.java
@@ -119,3 +119,34 @@ class printExamples {
     System.out.format("%2d %2d %2d\n", 8, 12, 16);
   }
 }
+
+// drawSmile function
+import com.educative.graphics.*;
+
+class Smile {
+  public static void drawSmile(Canvas c, int x, int y, double scale) {
+    // Draw the outline of the face
+    c.fill("yellow");
+    c.stroke("black");
+    c.circle(x, y, 50 * scale);
+
+    // draw the mouth
+    c.stroke("black");
+    c.fill("yellow");
+    c.circle(x, y, 30 * scale);
+    c.stroke("yellow");
+    c.rect(x - 32 * scale, y - 32 * scale, 62 * scale, 40 * scale);
+
+    // draw the eyes
+    c.stroke("black");
+    c.fill("black");
+    c.circle(x - 20 * scale, y - 10 * scale, 5 * scale);
+    c.circle(x + 20 * scale, y - 10 * scale, 5 * scale);
+  }
+
+  public static void main(String[] args) {
+    Canvas c;
+    c = new Canvas(200, 200);
+    drawSmile(c, 100, 100, 0.5);
+  }
+}
